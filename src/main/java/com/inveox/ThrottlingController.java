4410d981875d85d058c45efec86c6615f49628c4
sergiy <sk@linux.com>
2021-07-29 09:15:35 +0200

    not finished

--- src/main/java/com/inveox/ThrottlingController.java
+++ src/main/java/com/inveox/ThrottlingController.java
@@ -1,11 +1,19 @@
 package com.inveox;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalTime;
+
 public class ThrottlingController {
 
     private final int ratePerSecond;
+    private int counterPerSecond = 0;
+    private long currentSecond;
+    private CurrentTime currentTime;
 
-    public ThrottlingController(int ratePerSecond) {
+    public ThrottlingController(int ratePerSecond, CurrentTime currentTime) {
         this.ratePerSecond = ratePerSecond;
+        this.currentTime = currentTime;
     }
 
     /**
@@ -23,7 +31,34 @@ public class ThrottlingController {
      * @return whether call should be throttled (true) or not (false)
      */
     boolean shouldThrottle(MachinePayload payload) {
-        //TODO
+    
+    
+        long epochSecond = Instant.now().getEpochSecond();
+        if (counterPerSecond > ratePerSecond && currentTime.isSameSecond(epochSecond))  {
+            return true;
+        }
+        
+        counterPerSecond++;
+        
+        currentTime.set(epochSecond);
+
         return false;
+    }
+    
+    public static class CurrentTime {
+        private long currentSecond;
+        
+        public CurrentTime() {
+            this.currentSecond = Instant.now().getEpochSecond();
+            
+        }
+        
+        public void set(long currentSecond) {
+            this.currentSecond = currentSecond;
+        }
+        
+        public boolean isSameSecond(long epochSecond) {
+            return currentSecond == epochSecond;
+        }
     }
 }
--- src/test/java/com/inveox/ThrottlingControllerTest.java
+++ src/test/java/com/inveox/ThrottlingControllerTest.java
@@ -2,25 +2,44 @@ package com.inveox;
 
 import org.junit.jupiter.api.Test;
 
-
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.anyLong;
 import static org.mockito.Mockito.mock;
 
 public class ThrottlingControllerTest {
-
-
+    
+    
     @Test
     void shouldFailWhenExceedsRate() {
         //given
-        ThrottlingController controller = new ThrottlingController(1);
+        ThrottlingController.CurrentTime currentTime = mock(ThrottlingController.CurrentTime.class);
+        given(currentTime.isSameSecond(anyLong())).willReturn(true);
+        ThrottlingController controller = new ThrottlingController(1, currentTime);
         MachinePayload payload = mock(MachinePayload.class);
         controller.shouldThrottle(payload);
-
+        
+        
         //when
         boolean throttle = controller.shouldThrottle(payload);
-
+        
         //then
         assertTrue(throttle);
     }
-
+    
+    
+    @Test
+    void shouldPassFirstCall() {
+        //given
+        ThrottlingController controller = new ThrottlingController(1, new ThrottlingController.CurrentTime());
+        MachinePayload payload = mock(MachinePayload.class);
+        
+        //when
+        boolean throttle = controller.shouldThrottle(payload);
+        
+        //then
+        assertFalse(throttle);
+    }
+    
 }
