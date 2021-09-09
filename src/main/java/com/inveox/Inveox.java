00ee93940450c597ae525e8dfc1df7e225dcd542
Rafal Jaworski <rafal.jaworski@inveox.com>
2021-09-09 09:31:47 +0200

    add test

--- src/main/java/com/inveox/Inveox.java
+++ src/main/java/com/inveox/Inveox.java
@@ -2,7 +2,7 @@ package com.inveox;
 
 public class Inveox {
 
-    public static void main(String[] args) {
-        System.out.println("Hello Inveox!");
+    public String hello() {
+        return "inveox";
     }
 }
--- src/test/java/com/inveox/InveoxTest.java
+++ src/test/java/com/inveox/InveoxTest.java
@@ -2,10 +2,14 @@ package com.inveox;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class InveoxTest {
+
+    Inveox inveox = new Inveox();
 
     @Test
     public void test() {
-
+        assertThat(inveox.hello()).isEqualTo("inveox");
     }
 }
